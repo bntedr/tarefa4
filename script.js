1546f1eec1938d0fb2d8278bb93202298d788ed9
bntedr <bentoeduardo120@gmail.com>
2024-06-29 14:04:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//Exerício1
+let cargo = "diretor"
+switch (cargo) {
+    case "gerente":
+        console.log(`Você tem permissão de ${cargo}`)
+        break
+    case "comum":
+        console.log(`Você tem permissão de ${cargo}`)
+        break
+    case "diretor":
+        console.log(`Você tem permissão de ${cargo}`)
+        break
+    default:
+        console.log("Permissão Inválida!")
+
+//Exercício2
+        let senha= 1234
+        if(senha === 1234){
+            console.log("ACESSO PERMITIDO")
+        }else{
+            console.log("ACESSO NEGADO")
+        }
